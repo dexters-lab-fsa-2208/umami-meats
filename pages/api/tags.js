f6bb43361d89e6c8e5bbb36351bf2e6aa008b04c
Daniel Margolin <dannyjmargolin@gmail.com>
2022-09-20 16:59:08 -0400

    tags api

---
+++ pages/api/tags.js
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Tag } from '../../server/db';
+
+export default async function handler(req, res) {
+	if (req.method === 'GET') {
+		try {
+			const tags = await Tag.findAll();
+			console.log(tags);
+			res.json(tags);
+		} catch (err) {
+			res.status(500).json({ error: err.message });
+		}
+	}
+	if (req.method === 'POST') {
+		try {
+			console.log(req.body);
+			const tag = await Tag.create(req.body);
+			res.status(201).json(tag);
+		} catch (err) {
+			res.status(500).json({ error: err.message });
+		}
+	}
+}
