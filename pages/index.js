3585dd5976ad4088d283212952b511241a0d5149
byoon97 <byoon5397@gmail.com>
2022-09-20 15:31:00 -0400

    added some products to home page to test

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,53 @@
 import React from "react";
-import {
-  useGetUsersQuery,
-  useGetProductsQuery,
-} from "../src/redux/reducers/apiSlice";
+import styled from "styled-components";
+import { useGetProductsQuery } from "../src/redux/reducers/apiSlice";
+import Nav from "../src/components/Nav";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const NavContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const HeroContainer = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+`;
+
+const ProductImgContainer = styled.img`
+  height: 30em;
+  width: 30em;
+`;
+
+const ProductContainer = styled.div`
+  display: flex;
+`;
+// Component starts here!!!
 
 export default function Home() {
-  // const { data } = useGetProductsQuery;
-  const { data } = useGetUsersQuery;
+  const { data, isLoading } = useGetProductsQuery();
+
   console.log(data);
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Nav />
+      <HeroContainer>
+        {isLoading ? (
+          <div>products are loading</div>
+        ) : (
+          data.map((ele) => (
+            <ProductContainer key={ele.id}>
+              <div>{ele.name}</div>
+              <ProductImgContainer src={ele.img} />
+            </ProductContainer>
+          ))
+        )}
+      </HeroContainer>
+    </Container>
+  );
 }
