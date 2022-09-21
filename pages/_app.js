81483ad5169495dd95ba3a57680cdb7743ea963c
l-ohman <jakelohman7@gmail.com>
2022-09-21 13:39:46 -0500

    created components folder, restructured files for main layout

--- pages/_app.js
+++ pages/_app.js
@@ -4,74 +4,27 @@ import { apiSlice } from "../src/redux/reducers/apiSlice";
 import { store } from "../src/redux/store/store";
 
 import styled from "styled-components";
-import { FaPhone, FaShoppingCart, FaUser, FaSearch } from 'react-icons/fa';
+import { Header, Footer } from "../src/components";
 
 const MainContainer = styled.div`
-  height: 100vh;
+  position: relative;
+  min-height: 100vh;
   * {
     margin: 0;
+    user-select: none;
   }
 `;
 
-const HeaderContainer  = styled.div`
-  color: white;
-  height: 10%;
-`;
-const HeaderTop = styled.div`
-  background-color: black;
-  display: flex;
-  justify-content: flex-end;
-  height: 30%;
-`;
-const HeaderMain = styled.div`
-  background-color: darkred;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  height: 70%;
-`;
-
-const Footer = styled.div`
-  position: fixed;
-  color: white;
-  width: 100%;
-  background-color: darkred;
-  bottom: 0;
-  height: 1.5em;
-  display: flex;
-  justify-content: flex-end;
-  margin-bottom: -1px;
-`;
-
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return (
     <Provider api={apiSlice} store={store}>
       <MainContainer>
-
-        {/* HEADER */}
-        <HeaderContainer>
-          <HeaderTop>
-            <FaUser /> Account <FaShoppingCart /> Cart
-          </HeaderTop>
-          <HeaderMain>
-            <h1>Logo</h1>
-            <h1>Steaks</h1>
-            <h1>Sushi</h1>
-            <FaSearch />
-          </HeaderMain>
-        </HeaderContainer>
-
-        {/* MAIN CONTENT */}
+        <Header />
         <Component {...pageProps} />
-
-        {/* FOOTER */}
-        <Footer>
-          <FaPhone /> Contact 
-        </Footer>
-        
+        <Footer />
       </MainContainer>
     </Provider>
   );
 }
 
-export default MyApp;
+export default App;
