830cf597cc818c98873e204ebd02b513710dc6bb
byoon97 <byoon5397@gmail.com>
2022-09-22 11:55:09 -0400

    added cart and user slices

---
+++ src/redux/reducers/cart-slice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    cart: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      state.cart.push(action.payload);
+    },
+    removeFromCart: (state, action) => {
+      state.cart.splice(action.payload.id, 1);
+    },
+    clearCart: (state) => {
+      state.cart = [];
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
+export default cartSlice;
