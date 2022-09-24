ee4c96a1a7b74f7e901aee933138ab7e8bc0e960
Daniel Margolin <dannyjmargolin@gmail.com>
2022-09-23 20:54:46 -0400

    updated addtocart reducer to handle mulitple clicks of the same product, did half the checkout page, added rounding to .2 decimals

--- src/redux/reducers/cart-slice.js
+++ src/redux/reducers/cart-slice.js
@@ -8,7 +8,14 @@ const cartSlice = createSlice({
   reducers: {
     addToCart: (state, action) => {
       console.log("hitting reducer");
-      state.cart.push(action.payload);
+      let found = false;
+      state.cart.map((obj, idx) => {
+        if (obj.name === action.payload.name){
+          state.cart[idx].quantity += action.payload.quantity
+          found = true;
+        }
+      });
+      !found && state.cart.push(action.payload)
     },
     removeFromCart: (state, action) => {
       state.cart.splice(action.payload.id, 1);
