cb4735a2252fff4863fafc22b31a7edee45aa64a
l-ohman <jakelohman7@gmail.com>
2022-09-26 12:55:19 -0500

    updated CSS for products in product list

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { FaPhone, FaShoppingCart, FaUser, FaSearch } from "react-icons/fa";
 import {
   useGetProductsQuery,
   useCreateOrderMutation,
@@ -10,21 +9,14 @@ import {
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../redux/reducers/cart-slice";
+import { Loading } from "./";
 
-// const tags = [
-// 	{ tagName: 'tuna' },
-// 	{ tagName: 'beef' },
-// 	{ tagName: 'japanese' },
-// 	{ tagName: 'american' },
-//     { tagName: 'sushi' }
-// ];
 const BodyContainer = styled.div`
   display: flex;
 `;
 
+// TAGS
 const TagContainer = styled.div`
-  // width: 100%;
-  // width 200px;
   display: flex;
   flex-direction: column;
 `;
@@ -32,38 +24,58 @@ const TagName = styled.p`
   width: 100%;
 `;
 
+// PRODUCTS
 const ProductsContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-evenly;
-  // margin-left: 15%;
 `;
 
-const ProductTitle = styled.h2`
+const ProductListHeader = styled.h2`
   flex: 2 2 200%;
   text-align: center;
+  margin-top: 0.3em;
 `;
 
+// SINGLE PRODUCT IN LIST
 const Product = styled.div`
-  width: 150px;
-  height: 200px;
+  width: 175px;
+  max-width: 65%;
+  min-width: 175px;
+  min-height: 225px;
+
+  background-color: rgb(230, 230, 230);
+  box-shadow: 1px 1px 7px rgba(100, 100, 100, 0.43);
+
+  margin: 0.5em;
   flex: 1 1 auto;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  button {
+  justify-content: space-between;
+
+  img {
+    /* using 'height' alone does not make some images the required height */
+    min-height: 150px;
+    max-height: 150px;
+    width: 150px;
+
+    object-fit: cover;
+    margin: 0.7em auto 0.15em;
+
+    box-shadow: 1px 1px 6px rgba(100, 100, 100, 0.31);
   }
-`;
-const ProductImage = styled.img`
-  width: 150px;
-  height: 150px;
-`;
-const ProductName = styled.p`
-  text-align: center;
-  font-size: 12px;
-  span {
-    color: red;
+  p {
+    text-align: center;
+    font-size: 0.85em;
+    max-width: 75%;
+    margin: 0.15em auto;
+
+    &.productPrice {
+      color: red;
+    }
+  }
+  button {
+    margin: 0.15em auto 0.7em;
   }
 `;
 
@@ -89,80 +101,84 @@ export default function Products({ products, isLoading }) {
     console.log(filteredProducts);
   };
 
-  return (
-    <BodyContainer>
-      <TagContainer>
-        {products &&
-          products.length &&
-          isSuccess &&
-          tags
-            .filter((tag) => tag.tagType === products[0].type)
-            .map((tag) => (
-              <TagName onClick={(e) => tagFilter(tag.tagName)} key={tag.id}>
-                {tag.tagName.charAt(0).toUpperCase() + tag.tagName.slice(1)}
-              </TagName>
-            ))}
-      </TagContainer>
-      {
-        //TODO CHANGE PRODUCTS && TO ISLOADING ? BY MOVING TERNARY HERE
-      }
-      <ProductsContainer>
-        <ProductTitle>
-          Our{" "}
+  if (isLoading) {
+    return <Loading />;
+  } else
+    return (
+      <BodyContainer>
+        <TagContainer>
           {products &&
             products.length &&
-            products[0].type.charAt(0).toUpperCase() +
-              products[0].type.slice(1)}
-        </ProductTitle>
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : (
-          (filtered ? filteredProducts : products).map((product) => (
-            <Product key={product.id}>
-              <Link href={`${product.type}/${product.id}`}>
-                <ProductImage src={product.img} />
-              </Link>
-              <Link href={`${product.type}/${product.id}`}>
-                <ProductName>
-                  {product.name} <span>{product.price + "/lb"}</span>
-                </ProductName>
-              </Link>
+            isSuccess &&
+            tags
+              .filter((tag) => tag.tagType === products[0].type)
+              .map((tag) => (
+                <TagName onClick={(e) => tagFilter(tag.tagName)} key={tag.id}>
+                  {tag.tagName.charAt(0).toUpperCase() + tag.tagName.slice(1)}
+                </TagName>
+              ))}
+        </TagContainer>
+        {
+          //TODO CHANGE PRODUCTS && TO ISLOADING ? BY MOVING TERNARY HERE
+        }
+        <ProductsContainer>
+          <ProductListHeader>
+            {"Our "}
+            {products &&
+              products.length &&
+              products[0].type.charAt(0).toUpperCase() +
+                products[0].type.slice(1)}
+          </ProductListHeader>
+          {isLoading ? (
+            <div>Loading...</div>
+          ) : (
+            (filtered ? filteredProducts : products).map((product) => (
+              <Product key={product.id}>
+                <Link href={`${product.type}/${product.id}`}>
+                  <img src={product.img} />
+                </Link>
+                <Link href={`${product.type}/${product.id}`}>
+                  <p className="productName">{product.name}</p>
+                </Link>
+                <p className="productPrice">{product.price + "/lb"}</p>
 
-              {/* if a user is logged in, onClick will post new line items
+                {/* if a user is logged in, onClick will post new line items
               if user is not logged in, dispatch to redux store */}
-              {isLoggedIn ? (
-                <button
-                  onClick={() =>
-                    createLineItem({
-                      orderId: cartId,
-                      productId: product.id,
-                      qty: 1,
-                    })
-                  }
-                >
-                  Add To Cart
-                </button>
-              ) : (
-                <button
-                  onClick={() =>
-                    dispatch(
-                      addToCart({
-                        id: cart.length - 1,
-                        name: product.name,
-                        image: product.img,
-                        price: product.price,
-                        quantity: 1,
+                {isLoggedIn ? (
+                  <button
+                    className="secondaryButton"
+                    onClick={() =>
+                      createLineItem({
+                        orderId: cartId,
+                        productId: product.id,
+                        qty: 1,
                       })
-                    )
-                  }
-                >
-                  Add To Cart
-                </button>
-              )}
-            </Product>
-          ))
-        )}
-      </ProductsContainer>
-    </BodyContainer>
-  );
+                    }
+                  >
+                    Add To Cart
+                  </button>
+                ) : (
+                  <button
+                    className="secondaryButton"
+                    onClick={() =>
+                      dispatch(
+                        addToCart({
+                          id: cart.length - 1,
+                          name: product.name,
+                          image: product.img,
+                          price: product.price,
+                          quantity: 1,
+                        })
+                      )
+                    }
+                  >
+                    Add To Cart
+                  </button>
+                )}
+              </Product>
+            ))
+          )}
+        </ProductsContainer>
+      </BodyContainer>
+    );
 }
