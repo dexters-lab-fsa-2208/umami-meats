a3e09f29ac165b415b4765c398e9b6cf2ac9e301
l-ohman <jakelohman7@gmail.com>
2022-09-28 21:51:48 -0500

    updated account view/edit pages, fixed admin verification

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -6,109 +6,120 @@ import authService from "../../src/services/auth.service";
 import { RemoveSSRFromComponent } from "../../src/utils";
 import { Loading } from "../../src/components";
 import {
-    ManageProducts,
-    ManagePromos,
-    ManageUsers,
-    ToolList,
-    UpdateFeatured,
-  } from "../../src/components/admin";
+  ManageProducts,
+  ManagePromos,
+  ManageUsers,
+  ToolList,
+  UpdateFeatured,
+} from "../../src/components/admin";
 
 const AdminToolsContainer = styled.div`
-    &#noAccess {
-        text-align: center;
-        margin: 2em 3em;
-        p {
-            margin-bottom: 1.5em;
-            &:last-child {
-                font-style: italic;
-            }
-        }
-    }
-    margin: 1em;
+  max-width: 700px;
+  margin: 1.2em auto;
 
-    h2 {
-        text-align: center;
-        margin-bottom: 0.5em;
-    }
-    .returnHome {
-        font-size: 1.1em;
+  &#noAccess {
+    text-align: center;
+    margin: 2em 3em;
+    p {
+      margin-bottom: 1.5em;
+      &:last-child {
         font-style: italic;
-        margin: 0 0.5em 0.5em;
+      }
     }
-    .returnTools {
-        font-style: italic;
-        margin: 0 0.5em 0.5em;
-        text-decoration: underline;
-    }
-    #toolHeader {
-        margin-top: 0.5em;
-    }
-`
+  }
+  /* margin: 1em; */
+
+  h2 {
+    text-align: center;
+    margin-bottom: 0.5em;
+  }
+  .returnHome {
+    font-size: 1.1em;
+    font-style: italic;
+    margin: 0 0.5em 0.5em;
+  }
+  .returnTools {
+    font-style: italic;
+    margin: 0 0.5em 0.5em;
+    text-decoration: underline;
+  }
+  #toolHeader {
+    margin-top: 0.5em;
+  }
+`;
 
 function AdminTools() {
-    const user = useSelector(state => state.user);
-
-    // ADMIN VERIFICATION
-    const [loading, setLoading] = useState(true);
-    const [userVerified, setUserVerified] = useState(false);
-    // checks token in redux state to ensure that the user is admin
-    useEffect(() => {
-        const verify = async (token) => {
-            const { verifyToken } = authService;
-            return await verifyToken(token);
-        }
-        if (user?.user?.admin) {
-            let token = JSON.parse(localStorage.user).token;
-            if (verify(token)) {
-                setUserVerified(true);
-                setLoading(false);
-            }
-        } else {
-            setLoading(false);
-        }
-    }, [user])
-
-    // TOOL SELECTION
-    const [selectedTool, setSelectedTool] = useState("");
-
-    const renderSelectedTool = () => {
-        // cases: featured, products, promos, users
-        switch (selectedTool) {
-            case ("featured"):
-                return <UpdateFeatured />
-            case ("products"):
-                return <ManageProducts />
-            case ("promos"):
-                return <ManagePromos />
-            case ("users"):
-                return <ManageUsers />
-            default:
-                return <ToolList select={setSelectedTool} />
-        }
+  // ADMIN VERIFICATION
+  const [loading, setLoading] = useState(true);
+  const [userVerified, setUserVerified] = useState(false);
+  // checks token to ensure that the user is admin
+  useEffect(() => {
+    const verify = async (token) => {
+      const { verifyToken } = authService;
+      return await verifyToken(token);
+    };
+    if (localStorage?.user) {
+      let token = JSON.parse(localStorage.user).token;
+      if (verify(token)) {
+        setUserVerified(true);
+        setLoading(false);
+      }
+    } else {
+      setLoading(false);
     }
-    
-    if (loading) {
-        return <Loading message={"Verifying admin..."} />
-    } else if (!loading && !userVerified) {
-        return (
-            <AdminToolsContainer id="noAccess">
-              <p>You do not have the credentials to access this page. If you think this is a mistake, please try logging out and logging back in.</p>
-              <Link href="/">
-                <p className="returnHome">Return to <u>home page</u></p>
-              </Link>
-            </AdminToolsContainer>
-        )
-    } else
-    return(
-        <AdminToolsContainer>
-            {selectedTool ? <>
-            <p onClick={() => setSelectedTool("")} className="returnTools">Return to all tools</p>
+  }, []);
+
+  // TOOL SELECTION
+  const [selectedTool, setSelectedTool] = useState("");
+
+  const renderSelectedTool = () => {
+    // cases: featured, products, promos, users
+    switch (selectedTool) {
+      case "featured":
+        return <UpdateFeatured />;
+      case "products":
+        return <ManageProducts />;
+      case "promos":
+        return <ManagePromos />;
+      case "users":
+        return <ManageUsers />;
+      default:
+        return <ToolList select={setSelectedTool} />;
+    }
+  };
+
+  if (loading) {
+    return <Loading message={"Verifying admin..."} />;
+  } else if (!loading && !userVerified) {
+    return (
+      <AdminToolsContainer id="noAccess">
+        <p>
+          You do not have the credentials to access this page. If you think this
+          is a mistake, please try logging out and logging back in.
+        </p>
+        <Link href="/">
+          <p className="returnHome">
+            Return to <u>home page</u>
+          </p>
+        </Link>
+      </AdminToolsContainer>
+    );
+  } else
+    return (
+      <AdminToolsContainer>
+        {selectedTool ? (
+          <>
+            <p onClick={() => setSelectedTool("")} className="returnTools">
+              Return to all tools
+            </p>
             <h2 id="toolHeader">Placeholder</h2>
-            </> : <h2>Administrator Tools</h2>
-            }
-            {renderSelectedTool()}
-        </AdminToolsContainer>
-    )
+          </>
+        ) : (
+          <h2>Administrator Tools</h2>
+        )}
+        {renderSelectedTool()}
+      </AdminToolsContainer>
+    );
 }
 
 export default RemoveSSRFromComponent(AdminTools);
