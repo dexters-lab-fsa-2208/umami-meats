e199cbabcb9638c3fadd250f29b0268af44e1801
l-ohman <jakelohman7@gmail.com>
2022-12-15 16:07:19 -0600

    Updated DB connection

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,8 @@
 const Sequelize = require("sequelize");
 const pg = require("pg");
 
+let db;
+
 const config = {
   port: 5432,
   dialect: "postgres",
@@ -16,16 +18,14 @@ const config = {
   // logging: false,
 };
 
-let db;
-
 if (process.env.DB_NAME) {
 	config.database = process.env.DB_NAME;
 	config.username = process.env.DB_USER;
 	config.password = process.env.DB_PASS;
 	config.host = process.env.DB_HOST;
-  db = new Sequelize(process.env.DB_HOST, config)
+  db = new Sequelize(config);
 } else {
-  db = new Sequelize("postgres://localhost/dexter", config)
+  db = new Sequelize("postgres://localhost/dexter", config);
 }
 
 module.exports = db;
